2dec08bff181882439b6bcc9236e15cb833468a5
Fatih Erikli <fatiherikli@gmail.com>
2018-11-29 13:21:20 +0300

    I hate white spaces
    🦄

--- possibility.py
+++ possibility.py
@@ -1,9 +1,7 @@
 from random import choice
-
 __author__ = "LYK 2018 DJANGO"
 __license__ = "MIT License"
 __version__ = "1.0.0"
-
 class Possibility:
 """
 This module functions returns head or tail, rock or paper or scissors,
@@ -12,7 +10,6 @@ Usage:
 dice_roll(numbers of dice)
 head_tail()
 rock_paper_scissors()
-
 """
 def dice_roll(self, how_many_dice):
 """
@@ -23,17 +20,15 @@ list_dice_probability = []
 for i in range(how_many_dice):
 list_dice_probability.append(choice(dice))
 return list_dice_probability
-
 def head_tail(self):
 """
 Head or Tail
 """
 return choice(["Head", "Tail"])
-
 def rock_paper_scissors(self):
 """
 Classic rock-paper-scissors for 2 people
 """
 first_person = choice(["Rock", "Paper", "Scissors"])
 second_person = choice(["Rock", "Paper", "Scissors"])
-return print("First Person  : {}\nSecond Person : {}".format(first_person, second_person))
+return print("""First Person  : {}\nSecond Person : {}""".format(first_person,second_person))
